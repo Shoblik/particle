5799cb22f105ab5bb37093e5c3ecf839314d360d
Simon Hoblik <shoblik@yahoo.com>
2024-05-12 16:43:26 -0700

    updating version number

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         </button>
         <div class="menu" id="menu">
             <div class="standard-menu-item">
-                <p>Version 1.64</p>
+                <p>Version 1.97</p>
             </div>
             <div class="standard-menu-item">
                 <label for="radiusSlider">Circle Radius = <span id="circleRadiusValDisplay"></span>px</label>
